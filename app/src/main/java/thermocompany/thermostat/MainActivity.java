32fe4d3a97c1a2eda8ea9d56c1e81ba0634c327f
Thomas Brocken <thomasbrocken@live.nl>
2016-05-31 12:43:58 +0200

    Plus and minus works with the server

--- app/src/main/java/thermocompany/thermostat/MainActivity.java
+++ app/src/main/java/thermocompany/thermostat/MainActivity.java
@@ -2,12 +2,97 @@ package thermocompany.thermostat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.net.ConnectException;
+
+import util.*;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView temp;
+    double currentTemperature;
+    Button plus;
+    Button minus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        HeatingSystem.BASE_ADDRESS = "http://pcwin889.win.tue.nl/2id40-ws/60";
+        temp = (TextView)findViewById(R.id.temp);
+        plus = (Button)findViewById(R.id.plus);
+        minus = (Button)findViewById(R.id.minus);
+
+
+
+
+        // for some internet things we need to use Threads, don't know why
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    currentTemperature = Double.parseDouble(HeatingSystem.get("currentTemperature"));
+                    temp.setText(String.valueOf(currentTemperature));
+                } catch (ConnectException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+
+
+        plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentTemperature = (currentTemperature*10+1)/10; // to prevent rounding issues
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            HeatingSystem.put("currentTemperature", String.valueOf(currentTemperature));
+                            temp.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    temp.setText(String.valueOf(currentTemperature));
+
+                                }
+                            });
+                        } catch (InvalidInputValueException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        });
+
+        minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                currentTemperature = (currentTemperature*10-1)/10;
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            HeatingSystem.put("currentTemperature", String.valueOf(currentTemperature));
+                            temp.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    temp.setText(String.valueOf(currentTemperature));
+
+                                }
+                            });
+                        } catch (InvalidInputValueException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        });
     }
 }
